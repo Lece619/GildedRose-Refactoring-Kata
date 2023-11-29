621eaa56ffe306713046d340b469b4b161b67ae1
Lece <lece619@gmail.com>
2023-11-29 21:52:31 +0900

    test: record의 사용과 현재 단계 1의 문제점 파악
    record 정적 필드이기 때문에 조건에 따라 변경하는 값은 넣어주면 해당이 안되고 초기화 동작후 값이 변하지 않는다.

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -2,61 +2,38 @@ package com.gildedrose;
 
 class GildedRose {
     Item[] items;
-
     public GildedRose(Item[] items) {
         this.items = items;
     }
-
+    // TODO TEST 코드 작성
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
 
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
+            Item item = items[i];
+            ItemComparisonRecord comparisonRecord = new ItemComparisonRecord(item);
+            if (comparisonRecord.isLegendItem()) continue;
 
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
+            if (comparisonRecord.isNormalAndPositiveQuality()) item.quality = comparisonRecord.isSellInNegativeNumber() ?
+                item.quality-2 :
+                item.quality-1;
 
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
+            // TODO 추가된 변경 사항
+            if (comparisonRecord.isSoBadAndPositiveQuality()) item.quality = comparisonRecord.isSellInNegativeNumber() ?
+                item.quality-4 :
+                item.quality-2;
 
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
-                }
-            }
+            if (comparisonRecord.isSpecialAndQualityUnder50()) item.quality++;
+
+            if (comparisonRecord.isBackStageProcess()) {
+                if (item.sellIn < 11) item.quality++;
+                if (item.sellIn < 6) item.quality++;
+            } else if (comparisonRecord.isBackStageProcessInCaseNegativeSellIn()) item.quality = 0;
+
+
+            if (comparisonRecord.isAgedBrieProcess()) item.quality++;
+
+
+            item.sellIn--;
         }
     }
 }
