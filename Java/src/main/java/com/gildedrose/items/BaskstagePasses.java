9b4570ce1d23a0665f62c163d5c762c72adadca6
L <lece619@gmail.com>
2023-11-30 15:36:03 +0900

    Update BaskstagePasses.java

--- Java/src/main/java/com/gildedrose/items/BaskstagePasses.java
+++ Java/src/main/java/com/gildedrose/items/BaskstagePasses.java
@@ -5,6 +5,8 @@ import com.gildedrose.StoredItem;
 public class BaskstagePasses extends StoredItem {
 
     private static final int QUALITY_ZERO = 0;
+    private static final int QUALITY_INCREASE_5DAYS_BEFORE = 3;
+    private static final int QUALITY_INCREASE_10DAYS_BEFORE = 2;
 
     public BaskstagePasses(String name, int sellIn, int quality) {
         super(name, sellIn, quality);
@@ -12,16 +14,18 @@ public class BaskstagePasses extends StoredItem {
 
     @Override
     public void changeQuality() {
-
-        increaseQuality(QUALITY_INCREASE_DEFAULT_QUANTITY);
+        
+        if (stageStartBefore(5)) {
+            increaseQuality(QUALITY_INCREASE_5DAYS_BEFORE);
+            return;
+        }
 
         if (stageStartBefore(10)) {
-            increaseQuality(QUALITY_INCREASE_DEFAULT_QUANTITY);
+            increaseQuality(QUALITY_INCREASE_10DAYS_BEFORE);
+            return;
         }
 
-        if (stageStartBefore(5)) {
-            increaseQuality(QUALITY_INCREASE_DEFAULT_QUANTITY);
-        }
+        increaseQuality(QUALITY_INCREASE_DEFAULT_QUANTITY);       
     }
 
     private boolean stageStartBefore(int days) {
